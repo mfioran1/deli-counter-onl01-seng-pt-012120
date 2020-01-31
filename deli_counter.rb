842a140ebc207d9acf1a0bf7ce98ddade8a35357
Maurice Fiorani <mauricef97@gmail.com>
2020-01-31 03:26:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(deli)
  puts "The line is currently empty."
  else
    str = "The line is currently:"
-   each.with_index(1) do |person, i|
+   deli.each.with_index(1) do |person, i|
      str += "#{i}. #{person}"
    end
    puts str
